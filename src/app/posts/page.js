9c796ced9b4f714a7ddc777be035fcf4d0902e87
rjglozano <ryan.rjl38@gmail.com>
2023-11-28 18:53:50 +0800

    backend using nextjs

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,11 +1,27 @@
+"use client"
+
+
 import React from 'react'
-import Post from '../components/Post'
+import Post from '../../components/Post'
+import { useFetch } from 'usehooks-ts'
 
 const Posts = () => {
+  const results = useFetch('/api/posts/')
+
+  if(results.error){
+    return(
+      <div>
+        {results.error.message}
+      </div>
+    )
+  }
+
   return (
     <div>
-      Create a new post
-   </div>
+      {results.data?.map(post =>(
+        <Post key={post.id} title={post.title} author={post.author} body={post.body} />
+      ))}
+     </div>
   )
 }
 
