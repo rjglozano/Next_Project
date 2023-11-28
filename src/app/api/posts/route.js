c8a019b63c7e45aecc3791f681aa93e92d44edc3
rjglozano <ryan.rjl38@gmail.com>
2023-11-28 19:36:54 +0800

    post

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,3 +4,13 @@ export async function GET(request){
     const data = posts
     return new Response(JSON.stringify(data))
 }
+
+export async function POST(request){
+    const data = await request.json()
+
+    console.log(data)
+    return new Response(JSON.stringify({message:"post successfully"}))
+}
+
+
+
--- src/app/posts/new/page.js
+++ src/app/posts/new/page.js
@@ -1,14 +1,46 @@
-import React from 'react'
-import Post from '../../../components/Post'
-import { useRouter } from 'next/router'
+'use client'
 
-const Posts = ({params}) => {
+import Button from '@/components/Button'
+import React, { useState } from 'react'
+
+const Posts = () => {
+
+  const [form, setForm] = useState({
+    title: '',
+    author: '',
+    body: '',
+  })
+
+
+  const handleChange = (e) =>{
+    const id = e.target.id
+
+    setForm({
+      ...form,
+      [id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async(e)=>{
+    const response = await fetch('/api/posts/', {
+      method: 'POST',
+      body: JSON.stringify(form)
+    })
+
+    const data = await response.json();
+    console.log(data)
+  }
+
   return (
-    <div>
-      Create a new post
-      {/* title
-      author
-      Body */}
+    <div className='flex flex-col'>
+      <label htmlFor="title">Title</label>
+      <input type="text" id='title' value={form.title} onChange={handleChange}/>
+      <label htmlFor="author">Author</label>
+      <input type="text" id='author' value={form.author} onChange={handleChange}/>
+      <label htmlFor="body">Body</label>
+      <input type="text" id='body' value={form.body} onChange={handleChange}/>
+
+      <button onClick={handleSubmit}>Submit</button>    
     </div>
   )
 }
